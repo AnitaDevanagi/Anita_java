26232b9400971a54d2e9fc468326c719776daad0
unknown <anitasddevanagi1@gmail.com>
2022-08-31 19:37:51 +0530

    sunita file

---
+++ sunita.java
@@ -0,0 +1,14 @@
+class Namasakara
+{
+    public void hand()
+{
+      System.out.println("This is Book");
+}
+      public static void main(String[] args)
+{
+         System.out.println("Hi guys");
+         Namasakara pq = new Namasakara();
+          pq.hand();
+            
+      }
+}
