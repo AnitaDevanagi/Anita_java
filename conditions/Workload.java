d56ea3976672f42c77ba99e24768f17cfa4f16a1
unknown <anitasddevanagi1@gmail.com>
2022-09-01 17:32:36 +0530

    if else conditions

---
+++ conditions/Workload.java
@@ -0,0 +1,14 @@
+class Work{
+         public static void main(String [] arg){
+         
+        string first_day_Work = exhausted;
+        
+        if (exhausted ){
+        System.out.println("The day work in the Office heavy so she get exhausted");
+        }
+        else{
+        System.out.println("The last day of work in the Office become habits not exhausted");     
+        }
+   }
+}
+        
---
+++ conditions/statements.java
@@ -0,0 +1,14 @@
+class Weather{
+     public static void main(String[] args){
+             
+       byte temp_Expected = 22;
+       byte temp_Reality = 32;
+       if(temp_Expected < temp_Reality){
+       System.out.println("Today temparature is very high");
+       }
+       else{
+       System.out.println("Today temparature is  low");
+     }
+  }
+
+}
