de8fd83f649138f45a7058faec59a628c07f8e4b
unknown <anitasddevanagi1@gmail.com>
2022-09-01 17:49:49 +0530

    Arrays

---
+++ arrays/Sumarrays.java
@@ -0,0 +1,19 @@
+class array {
+     public static void main (String [] ar)
+   {
+   int [] number = {2,5,8,22,23,45,23};
+    int abc =2;
+    
+   System.out.println("size of the array"+.length);
+    for (int i=0; i<=22; i++)
+     {
+     if(number [i]%2==0)
+     {
+      abc=abc + number[i];
+     
+    
+   System.out.println("sum of even number"+abc);
+}
+   
+  }
+}
