61250e2b6285d1f3668ed2750afabbfa77cf005b
unknown <anitasddevanagi1@gmail.com>
2022-09-01 17:39:54 +0530

    Datatypes

---
+++ Datatypes/abc.java
@@ -0,0 +1,23 @@
+class World {
+             
+         static byte a;
+         static short name;
+         static int homeNumber;
+         static long contactNo;
+         static float cgpa;
+         static double percentage;
+         static boolean cat;
+         static char lastName;
+         
+         public static void main(String[] arg){
+
+         System.out.println("The Default value of byte is:"+ a);
+         System.out.println("The Default value of short is:"+ name);
+         System.out.println("The Default value of int is:"+ homeNumber);
+         System.out.println("The Default value of long is:"+ contactNo);
+         System.out.println("The Default value of float is:"+ cgpa);
+         System.out.println("The Default value of double is :"+ percentage);
+         System.out.println("The Default value of boolean is:"+ cat);
+         System.out.println("The Default value of char is:"+ lastName);
+         }
+}
---
+++ Datatypes/detail.java
@@ -0,0 +1,29 @@
+class Bike{
+     public static void main(String [] honda){
+            System.out.println("The Bike is Royal Enfield");
+
+                byte petrol=10;
+                System.out.println(petrol);
+                  
+                short distriCode=32;
+                System.out.println(distriCode);
+              
+                int bikeNumber =3456;  
+                System.out.println(bikeNumber);
+  
+                long licenceNo =2453679;
+                System.out.println(licenceNo); 
+                    
+                float cgpa = 7.5f ;
+                System.out.println(cgpa);
+
+                double xy = 12.67d; 
+                System.out.println(xy);
+            
+                char firstName='A';
+                System.out.println(firstName);
+
+                boolean costly=true;
+                System.out.println(costly);
+           }
+}
