7b171a7abb6f25dd916a402f66c6c764071a8b4b
unknown <anitasddevanagi1@gmail.com>
2022-09-01 18:07:56 +0530

    Control flow statements

---
+++ loopingStatements/Loop.java
@@ -0,0 +1,14 @@
+class Number{
+        public static void main(String [] arg){
+             
+             
+             for(byte i=23;  i<10  ; i++){
+             if(i%2==0)
+             System.out.println("The given number is even:"+i);
+             else{
+             System.out.println("The given number is odd:"+i);
+             }
+          }
+       }
+    }
+
