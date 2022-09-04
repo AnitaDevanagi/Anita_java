8159b62e78107750728d1bdea551c56915198c0b
unknown <anitasddevanagi1@gmail.com>
2022-09-04 17:07:53 +0530

    Constructor and Constructor overloading

---
+++ Constructor/Home.java
@@ -0,0 +1,30 @@
+class Home {
+   String  name;
+   String  color; 
+   byte   noOfrooms;
+   
+ 
+   Home(){
+   }
+
+   Home(String nam, String col){
+    name=nam;
+    color=col;
+   }
+  
+   Home(String nam, byte rooms){
+    name=nam;
+    noOfrooms=rooms;
+    
+   
+   }
+}
+
+
+   
+
+   
+   
+   
+  
+   
