80e833fa5fd8f76824708e6364a714f2a5c96221
unknown <anitasddevanagi1@gmail.com>
2022-09-04 17:10:35 +0530

    Constructor and Constructor overloading

---
+++ Constructor/Tiger.java
@@ -0,0 +1,16 @@
+class Tiger {
+   String name;
+   String color;
+   int   tail;
+
+    Tiger(){
+      System.out.println("created constructor");
+    }
+
+      Tiger(String nam){
+      System.out.println("created constructor");
+         name=nam;
+         
+    }
+}
+  
