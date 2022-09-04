d2e564bde7fa679828ded43b3e4d3694b8860e8c
unknown <anitasddevanagi1@gmail.com>
2022-09-04 17:09:14 +0530

    Constructor and Constructor overloading

---
+++ Constructor/Program.java
@@ -0,0 +1,22 @@
+class Program {
+     String Proname;
+     String Developername;
+     String Firstname;
+     int  Year;
+   
+   Program(){
+   }
+
+   Program( String pron){
+   Proname=pron;
+  }
+  
+   Program( String pron,String dev,String fir,int year){
+   Proname=pron;
+   Developername=dev;
+   Firstname=fir;
+   Year=year;
+  }
+}
+   
+    
