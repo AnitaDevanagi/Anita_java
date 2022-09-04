795418874c0f989e70657a6eafb0f2fdd50db309
unknown <anitasddevanagi1@gmail.com>
2022-09-04 17:11:13 +0530

    Constructor and Constructor overloading

---
+++ Constructor/Tigerpro.java
@@ -0,0 +1,14 @@
+class Tigerpro {
+  public static void main(String[] args){
+    
+   	Tiger p1 = new Tiger();
+        System.out.println(p1);
+        System.out.println(p1.name+"\t"+p1.color+"\t"+p1.noOfpens);
+  
+       Tiger a2 = new Tiger("King");
+       System.out.println(a2);
+       System.out.println(a2.name+"\t"+a2.color+"\t"+a2.noOfpens);
+
+  }
+}  
+  
