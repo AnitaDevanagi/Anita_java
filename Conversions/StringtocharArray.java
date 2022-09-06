2a952a387eebb6e599a905d34c8a0e4086cba640
unknown <anitasddevanagi1@gmail.com>
2022-09-06 19:23:21 +0530

    Convert String to char

---
+++ Conversions/StringtocharArray.java
@@ -0,0 +1,11 @@
+class StringtocharArray{
+ public static void main(String[] args){
+  String c = "Restaurant";
+  char[] d = c.toCharArray();
+  for(int i=0; i<d.length;i++){
+   
+    System.out.println(d[i]);
+}
+}
+}
+  
