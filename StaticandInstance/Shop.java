35cf28ba03e700cff19415285040538d7764fed3
unknown <anitasddevanagi1@gmail.com>
2022-09-06 15:47:47 +0530

    Static and Instance variable in String Array new keywors is Used

---
+++ StaticandInstance/Shop.java
@@ -0,0 +1,25 @@
+class Shop {
+     static  String name="Rakesh trades";
+     static String location="Hubali";
+
+      String owner[]={"Ankita","Kalyani","Ravi","Abhi"};
+      int noOfworkers;
+      int income;
+      boolean womens;
+      float reviews;
+    
+    Shop(){
+    }
+   
+     Shop(int noworker,int inco,boolean women, float review){
+       noOfworkers=noworker;
+       income=inco;
+       womens=women;
+       reviews=review;
+     }
+}
+
+
+    
+
+      
