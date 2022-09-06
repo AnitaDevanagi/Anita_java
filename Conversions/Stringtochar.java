84a7ee1bdd2bddb9e58b7b3afdcdf6c9f496f301
unknown <anitasddevanagi1@gmail.com>
2022-09-06 19:19:33 +0530

    Convert String to char

---
+++ Conversions/Stringtochar.java
@@ -0,0 +1,10 @@
+class Stringtochar{
+ public static void main(String[] args){
+ String a = "java programming language";
+ for(int i=0;i<a.length();i++){
+ char b = a.charAt(i);
+ System.out.println(b);
+  }
+ }
+}
+  
