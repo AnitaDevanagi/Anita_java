624eca9e2a10d375cad9aadfc8a06ca2c66f65fc
unknown <anitasddevanagi1@gmail.com>
2022-09-06 19:24:52 +0530

    Replace char from string

---
+++ Conversions/Replace.java
@@ -0,0 +1,19 @@
+class Replace{
+ public static void main(String[] args){
+
+   String a = "Animal";
+   String name= a.replace('a','e');
+   System.out.println(name);
+  
+
+   String b = "The Animal name is Tiger";
+   String c= b.replace("is","was");
+   System.out.println(c);
+
+  String ab = "Hotel";
+  String cd = ab.replace("Hotel","Restaurant");
+  System.out.println(cd);
+
+
+ }
+}
