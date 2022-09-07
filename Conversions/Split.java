61af16f34a8176de4666eef1c5897f2dc26609d7
unknown <anitasddevanagi1@gmail.com>
2022-09-07 15:18:52 +0530

    Split the String

---
+++ Conversions/Split.java
@@ -0,0 +1,18 @@
+class Split {
+
+    
+    String a = "Hello-how-are-you";
+    String[] c = a.split("-"); 
+  
+
+ public static void main (String[] args){
+
+    Split sp = new Split();
+    for(int i=0; i<sp.c.length;i++){
+    System.out.println(sp.c[i]);
+     
+   }
+
+  }
+
+}
