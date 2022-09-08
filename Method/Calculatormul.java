300c506a7d9040e89a6f253306af9765092f6532
unknown <anitasddevanagi1@gmail.com>
2022-09-08 16:42:13 +0530

    Multiplying the values

---
+++ Method/Calculatormul.java
@@ -0,0 +1,27 @@
+class Calculatormul{
+    
+  static void multiplication(){
+    
+   int a = 25;
+   int b = 5;
+   int sum = a*b;
+   System.out. println(sum);
+    
+   int x = 30;
+   int y = 4;
+   int z = 8;
+   int total = x*y*z;
+   System.out. println(total);
+   
+   double h = 30.8d;
+   int l = 5;
+   double ghd = h*l;
+   System.out. println(ghd);
+   }
+  public static void main(String [] args){
+   Calculatormul.multiplication();
+
+ }
+}
+   
+   
